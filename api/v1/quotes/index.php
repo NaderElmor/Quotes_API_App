b3c5d8e793055fcd2c514453acf440cecc25a9c4
Nader Elmor <naderelmor17@gmail.com>
2020-01-10 19:51:53 +0200

    Quotes index page is configured

--- api/v1/quotes/index.php
+++ api/v1/quotes/index.php
@@ -1,6 +1,13 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With");
 
-    require_once("../../../config/Database.php");
+require_once "../../../config/Database.php";
+require_once "../../../models/Quote.php";
+require_once "../../../models/HttpResponse.php";
 
-    $db = new DataBase();
-    var_dump($db->fetchAll("select * from users"));
+$db = new Database();
+$quote = new Quote($db);
+$http = new HttpResponse();
